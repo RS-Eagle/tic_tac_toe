11ecd762cd6bec546570ae9dccb300a2e8d585ae
Raman Kumar Charak <withrkgaming78@gmail.com>
2024-09-16 18:15:45 +0530

    as

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -9,7 +9,7 @@ const io =  new Server(server,
     {
         cors: {
             origin: ["http://localhost:3000","http://localhost:4000","https://radiant-licorice-1e2e2f.netlify.app"],
-            methods: ["GET", "POST"],
+            methods: ["GET", "POST"], 
         },
 })
 
